278eb9db923c11bfdbc3ba57fbf5cdac4fddd250
mocquin <mocquin@me.com>
2019-04-06 22:14:58 +0200

    add reading version of package directly from the _version.py in the setup

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
-
+import os
 import pathlib
+import re
+
 from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+### reading version from _version.py file
+VERSIONFILE="physipy/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
@@ -12,7 +28,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="physipy",
-    version="0.1.0",
+    version=find_version("physipy", "__init__.py"),
     description="Manipulate physical quantities in Python",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -35,3 +51,6 @@ setup(
 #        ]
 #    },
 )
+
+#if __name__ == "__main__":
+#    print(verstr)
