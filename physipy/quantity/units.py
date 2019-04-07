b6a10678756ff4db66b91aaef5ecf705241db1fb
mocquin <mocquin@me.com>
2019-04-07 11:48:56 +0200

    create a all_units dict

--- physipy/quantity/units.py
+++ physipy/quantity/units.py
@@ -3,6 +3,7 @@ This module provides dictionnaries of units :
  - SI_units : all the seven base SI units
  - SI_units_prefixed : same, with the prefixed version
  - SI_derived_units : other accepted units
+ - SI_derived_units_prefixed : same, with the prefixed version
  - other_units : other various units
  - units : all of the above
 
@@ -129,6 +130,7 @@ other_units = {key: make_quantity(value, symbol=key) for key, value in other_acc
 
 units = {**SI_units_prefixed, **SI_derived_units, **other_units} #including base SI units to units dict
 
+all_units = {SI_units_prefixed, SI_derived_units_prefixed, other_units}
 
 del pi
 del Quantity, Dimension, SI_UNIT_SYMBOL, quantify, make_quantity
