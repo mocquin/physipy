9e6f0fb57a9b1a0d5c8540139a647659920374af
mocquin <mocquin@me.com>
2020-02-26 16:52:49 +0100

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -33,4 +33,8 @@ setup(
     keywords='physics physical unit units dimension quantity quantities',
     packages=find_packages(exclude=("tests")),
     include_package_data=True,
+    install_requires=["numpy", 
+                      "scipy", 
+                      "sympy",
+                     ]
 )
