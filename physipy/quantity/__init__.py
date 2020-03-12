ca4688dbb290d6db265a3abcfdaba5f89a6b1456
mocquin <mocquin@me.com>
2020-03-12 07:38:12 +0100

    add decorator to top package level

--- physipy/quantity/__init__.py
+++ physipy/quantity/__init__.py
@@ -4,7 +4,7 @@
 from .quantity import Dimension, Quantity
 from .quantity import DimensionError, SI_UNIT_SYMBOL
 from .quantity import quantify, make_quantity
-from .utils import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, decorate_with_various_unit
+from .utils import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, add_back_unit_param, decorate_with_various_unit
 
 from .calculus import interp, linspace, vectorize, integrate_trapz, qroot, qbrentq
 from .calculus import trapz, quad, dblquad, tplquad
