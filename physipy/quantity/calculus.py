1ac04820510930f31bfb8d093f7b82ff7122fce1
mocquin <mocquin@me.com>
2021-05-04 12:24:44 +0200

    add trapz2 helper

--- physipy/quantity/calculus.py
+++ physipy/quantity/calculus.py
@@ -190,6 +190,31 @@ def brentq(func_cal, start, stop, *oargs, args=(), **kwargs):
     return Quantity(res, start_dim)
 
 
+def trapz2(Zs, ech_x, ech_y):
+    """
+    2D integral based on trapz.
+    ech_x is horizontal sampling, along row
+    ech_y is vertical sampling, along column
+    
+    
+    Example : 
+    ---------
+        #sample a 2 squared meter, in both direction with different spacing
+        nx = 12
+        ny = 30
+        ech_dx = np.linspace(0*m, 2*m, num=nx)
+        ech_dy = np.linspace(0*m, 1*m ,num=ny)
+        X, Y = np.meshgrid(ech_dx, ech_dy)
+        # make a uniform ponderation
+        Zs = np.ones_like(X)
+        print(trapz2(Zs, ech_dx, ech_dy))
+        #prints 2 m**2
+    
+    """
+    int_x = np.trapz(Zs, axis=-1, x=ech_x)
+    int_xy = np.trapz(int_x, axis=-1, x=ech_y)
+    return int_xy
+
 def main():
     pass
 
