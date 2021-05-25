f8892d00e511062527b3a63e632cd77718e47044
mocquin <mocquin@me.com>
2021-05-25 22:50:00 +0200

    create a random module wrapping numpy random distributions

---
+++ physipy/random.py
@@ -0,0 +1,27 @@
+"""Wrapper of numpy.random distributions"""
+
+import numpy as np
+from physipy import quantify, Quantity, Dimension
+
+
+def poisson(lam=1.0, size=None):
+    lam = quantify(lam)
+    samples = np.random.poisson(lam.value, size=size)
+    return Quantity(samples, lam.dimension)
+
+def normal(loc=0.0, scale=1.0, size=None):
+    loc = quantify(loc)
+    scale = quantify(scale)
+    if not loc.dimension == scale.dimension:
+        raise DimensionError(loc.dimension, scale.dimension)
+    samples = np.random.normal(loc=loc.value, scale=scale.value, size=size)
+    return Quantity(samples, loc.dimension)
+    
+    
+def uniform(low=0.0, high=1.0, size=None):
+    low = quantify(low)
+    high = quantify(high)
+    if not low.dimension == high.dimension:
+        raise DimensionError(low.dimension, high.dimension)
+    samples = np.random.normal(low=low.value, high=high.value, size=size)
+    return Quantity(samples, low.dimension)
