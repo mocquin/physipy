496073034542ce0e22c8a58df3de151d9a6b0c98
mocquin <mocquin@me.com>
2021-12-11 12:15:08 +0100

    cleanup comments and lint in widgets

--- physipy/qwidgets/qipywidgets.py
+++ physipy/qwidgets/qipywidgets.py
@@ -6,7 +6,7 @@ from ipywidgets import Layout
 import traitlets
 from traitlets import TraitError
 
-from physipy import quantify, Dimension, Quantity, units, set_favunit, DimensionError
+from physipy import quantify, Dimension, Quantity, units, set_favunit, DimensionError, dimensionify
 
 
 class QuantityText(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
@@ -138,6 +138,11 @@ class FDQuantityText(QuantityText):
 
 
 class QuantitySlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
+    """
+    A slider wrapped in a box to the stores the numerical value,
+    and exposes a value that is the Quantity.
+    """
+    
     # dimension trait : a Dimension instance
     dimension = traitlets.Instance(Dimension, allow_none=False)
     # value trait : a Quantity instance
@@ -177,20 +182,24 @@ class QuantitySlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
         else:
             self.favunit = favunit
         self.value.favunit = self.favunit
+        
+        # validate min
         if min is not None:
             qmin = quantify(min)
             if not qmin.dimension == self.value.dimension:
                 raise DimensionError(qmin.dimension, self.value.dimension)
         else:
             qmin = Quantity(0.0, self.value.dimension)
-        # max value
+            
+        # validate max value
         if max is not None:
             qmax = quantify(max)
             if not qmax.dimension == self.value.dimension:
                 raise DimensionError(qmax.dimension, self.value.dimension)
         else:
             qmax = Quantity(100.0, self.value.dimension)        
-        # step value
+            
+        # validate step value
         if step is not None:
             qstep = quantify(step)
             if not qstep.dimension == self.value.dimension:
@@ -202,7 +211,7 @@ class QuantitySlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
         self.qmin = qmin
         self.qmax = qmax
         
-        # set text widget
+        # set slider widget
         self.slider = ipyw.FloatSlider(value=self.value.value,
                                        min=self.qmin.value,
                                        max=self.qmax.value,
@@ -218,11 +227,13 @@ class QuantitySlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
                                        #              border="solid red"),
                                       )
     
+        # observe slider value : for interaction with the widget
         def update_label_on_slider_change(change):
             self.value = Quantity(change.new, self.value.dimension, favunit=self.favunit)
             self.label.value = str(self.value)
         self.slider.observe(update_label_on_slider_change, names="value")
 
+        # observe self.value : when using the OOP interaction
         def update_slider_value(change):
             self.slider.value = change.new.value
         self.observe(update_slider_value, names="value")
@@ -239,10 +250,6 @@ class QuantitySlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
             self.children = [
                 self.slider,
             ]
-
-
-
-
 
 
 class QuantityTextSlider(QuantityText):
@@ -283,16 +290,6 @@ class QuantityTextSlider(QuantityText):
         ]
 
 
-        
-        
-from ipywidgets import Layout
-from traitlets import TraitError
-from physipy import quantify, Dimension, Quantity, units, set_favunit, DimensionError
-import ipywidgets as ipyw
-import traitlets
-from numpy import pi
-
-
 class QuantityRangeSlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
     """
     TODO : 
@@ -318,6 +315,9 @@ class QuantityRangeSlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
                  fixed_dimension=False, label=True,#placeholder="Type python expr",
                  favunit=None,
                  **kwargs):
+        """
+        kwargs are passed to Box.
+        """
         
         super().__init__(**kwargs)
 
@@ -387,7 +387,6 @@ class QuantityRangeSlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
         self.observe(update_slider_value, names="value")
         
         
-        
         if label:        
             self.children = [
                 self.rslider,
@@ -400,14 +399,6 @@ class QuantityRangeSlider(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
 
 
         
-
-#import ipywidgets as ipyw
-#import physipy
-#import traitlets
-from physipy import dimensionify, Dimension, Quantity, units
-#from ipywidgets import Layout
-
-
 class FavunitDropdown(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
     dimension = traitlets.Instance(Dimension)
     value = traitlets.Instance(Quantity, allow_none=True)
@@ -416,17 +407,14 @@ class FavunitDropdown(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
     def __init__(self, dimension=None, all_units=False,
                  **kwargs):
         super().__init__(**kwargs)
-        #super().__init__(**kwargs)
         
-        # pouvoir ajouter des favunits lors de l'initiatsation dans la liste
-
         self.dimension = dimensionify(dimension)
         self.units = units
         self.units["-"] = Quantity(1, Dimension(None), symbol="-")
         
         # list of available units
         if self.dimension == Dimension(None) or all_units:
-            self.favunit_str_list = [u_str for u_str, u_q in self.units.items() ]
+            self.favunit_str_list = [u_str for u_str, u_q in self.units.items()]
        # elif not all_units and self.dimension is not None:
        #     self.favunit_str_list = [u_str for u_str, u_q in self.units.items() if self.dimension == u_q.dimension]
         else:
@@ -466,130 +454,3 @@ class FavunitDropdown(ipyw.Box, ipyw.ValueWidget, ipyw.DOMWidget):
         
         
         
-        
-def ui_widget_decorate(inits_values, kind="Text"):
-    """
-    inits_values contains list of tuples : 
-     - quantity init value
-     - str description
-     - name from signature
-     
-     Example
-     -------
-     def disk_PSA_cart(x, y, R, h):
-         return x*y*R/h
-     
-     ui = ui_widget_decorate([("x", 1*m),
-                              ("y", 1*m),
-                              ("R", 1*m, "Radius"),
-                              ("h", 1*m, "distance")])(disk_PSA_carth)
-     
-     """
-    if kind == "Text":
-        w = QuantityText
-    elif kind == "TextSlider":
-        w = QuantityTextSlider
-    else:
-        raise ValueError()
-    
-    def decorator_func(func):
-        
-        # create a widget list for all inputs
-        qwidget_list = []
-        for initq in inits_values:
-            # if provided, use alias for param
-            if len(initq) == 3:
-                qwidget_list.append(w(initq[1], description=initq[2]))
-            # else use param name
-            else:
-                qwidget_list.append(w(initq[1], description=initq[0]))
-                
-        # wrap function to display result
-        def display_func(*args, **kwargs):
-            res = func(*args, **kwargs)
-            display(res)
-            return res
-
-        # wrap all inputs widgets in a VBox
-        input_ui = ipyw.VBox(qwidget_list)
-
-        # create output widget, using inputs widgets
-        out = ipyw.interactive_output(display_func,
-                                     {k:qwidget_list[i] for i, k in enumerate([l[0] for l in inits_values])})
-
-        # if func has a "name" attribute, create a Label for display, else use default function __name__
-        if hasattr(func, "name"):
-            wlabel = ipyw.Label(func.name + ":")
-        else:
-            wlabel = ipyw.Label(func.__name__ + ":")
-            
-        # if func has a "latex" attribute, append it to Label
-        if hasattr(func, "latex"):
-            wlabel = ipyw.HBox([wlabel, ipyw.Label(func.latex)])
-            
-        # wrap all ui with Labels, inputs, and result
-        ui = ipyw.VBox([wlabel, input_ui, out])
-
-        return ui        
-    return decorator_func
-    
-    
-    
-
-            
-        
-def ui_widget_decorate_from_annotations(func, kind="Text"):
-    """
-     Example
-     -------
-     def disk_PSA_cart(x:mm, y:mm, R:mm, h:mm)-> msr:
-         return x*y*R/h
-     
-     ui = ui_widget_decorate_from_annotations(disk_PSA_carth)
-     ui
-
-     """
-    
-    # recreating an inits_values list based on annotations
-    # then reusing ui_widget_decorate
-    
-    import inspect
-    sig = inspect.signature(func)
-    inits_values = []
-    
-    for k,v in sig.parameters.items():
-        inits_values.append((v.name, v.annotation, v.name))
-
-    # fyi : to get retun annotation
-    #sig.return_annotation
-    if not sig.return_annotation == inspect._empty:
-        func = set_favunit(sig.return_annotation)(func)
-    
-    return ui_widget_decorate(inits_values, kind=kind)(func)
-            
-    
-def FunctionUI(tab_name, function_dict, kind="Text"):
-    acc = ipyw.Accordion()
-    
-    sections_uis = []
-    i = 0
-    for section_name, section_functions_list in function_dict.items():
-        function_uis = []
-        # loop over all functions in section and generate ui
-        for func in section_functions_list:
-            ui = ui_widget_decorate_from_annotations(func, kind=kind)
-            function_uis.append(ui)
-        box = ipyw.VBox(function_uis)
-        sections_uis.append(box)
-        acc.set_title(i, section_name)
-        i+=1
-        
-    acc.children = sections_uis
-
-    
-    
-    tab = ipyw.Tab()
-    tab.children = [acc]
-    tab.set_title(0, tab_name)
-
-    return tab
