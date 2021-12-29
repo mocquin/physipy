14e547f2fdc30fa0bb4a298d696d281dbb29a9e2
mocquin <mocquin@me.com>
2021-12-29 08:03:03 +0100

    add some imports to top level

--- physipy/__init__.py
+++ physipy/__init__.py
@@ -6,12 +6,12 @@ from numpy import pi
 from ._version import __version__
 from . import quantity
 from .quantity import Quantity, Dimension, make_quantity, quantify, DimensionError, dimensionify
-from .quantity import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, decorate_with_various_unit, add_back_unit_param, array_to_Q_array
+from .quantity import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, decorate_with_various_unit, add_back_unit_param, array_to_Q_array, asqarray
 from .integrate import quad, dblquad, tplquad
 from .optimize import root, brentq
 from .quantity import vectorize
 from .quantity import m, kg, s, A, K, cd, mol, rad, sr
-from .quantity import SI_units, SI_units_prefixed, SI_derived_units, other_units, units, all_units
+from .quantity import SI_units, SI_units_prefixed, SI_derived_units, other_units, units, all_units, SI_derived_units_prefixed
 
 from .quantity import setup_matplotlib
 
--- physipy/quantity/__init__.py
+++ physipy/quantity/__init__.py
@@ -4,11 +4,11 @@
 from .quantity import Dimension, Quantity
 from .quantity import DimensionError, SI_UNIT_SYMBOL
 from .quantity import quantify, make_quantity, dimensionify
-from .utils import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, add_back_unit_param, decorate_with_various_unit, array_to_Q_array
+from .utils import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, add_back_unit_param, decorate_with_various_unit, array_to_Q_array, asqarray
 
 from .calculus import *
 
 from .plot import setup_matplotlib
 
 from .units import m, s, kg, A, cd, K, mol, rad, sr
-from .units import SI_units, SI_units_prefixed, SI_derived_units, other_units, units, all_units
+from .units import SI_units, SI_units_prefixed, SI_derived_units, other_units, units, all_units, SI_derived_units_prefixed
