461a6b7d0710ec8dbc1de3db4fa75ebe77ffadd2
mocquin <mocquin@me.com>
2022-10-24 08:24:17 +0200

    clean unittests for uncertainties 5 are not passing (std, n, derivatives, sqrt, sin)

--- test/test_wrapping_uncertainties.py
+++ test/test_wrapping_uncertainties.py
@@ -4,134 +4,143 @@ import unittest
 from physipy import m, Quantity, Dimension, rad
 import uncertainties as uc
 from uncertainties import umath
-
+import pandas as pd
+import time
 
 import numpy as np
 
 class TestWrappingUncertainties(unittest.TestCase):
+    
+    
+    def setUp(self):
+        self.startTime = time.time()
+        self.tottime = 0
+
+    def tearDown(self):
+        t = time.time() - self.startTime
+        self.tottime = self.tottime + t
+        print(f"{self.id():70} : {t:10.6f}")
+        self.times.append(t)
+        self.ids.append(str(self.id()))
+    
+    
+    @classmethod
+    def setUpClass(cls):
+        
+        cls.x = uc.ufloat(0.20, 0.01)
+        cls.x_q = cls.x * m
+        cls.y = uc.ufloat(0.20, 0.01)
+        cls.y_q = cls.y * m
+        cls.z = uc.ufloat(3, 0.01)
+        cls.z_q = cls.z * m        
+    
+        cls.times = []
+        cls.ids = []
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.df = pd.DataFrame.from_dict({
+            "time":cls.times,
+            "id":cls.ids,
+        }) 
 
     def test_creation_from_mul(self):
-        x = uc.ufloat(0.20, 0.01)
         
-        x_q_l = x * m
-        x_q_r = m * x
-        x_q   =  Quantity(x, Dimension('m'))
+        x_q_l = self.x * m
+        x_q_r = m * self.x
+        x_q   =  Quantity(self.x, Dimension('m'))
         
         self.assertEqual(x_q_l, x_q_r)
         self.assertEqual(x_q_l, x_q)
         self.assertEqual(x_q_r, x_q)
         
     def test_mul(self):
-        x = uc.ufloat(0.20, 0.01)
-        x_q = x*m
         
-        res = x_q*m
-        exp = Quantity(x, Dimension('m**2'))
+        res = self.x_q*m
+        exp = Quantity(self.x, Dimension('m**2'))
         self.assertEqual(res, exp)
         
-        res = m*x_q
-        exp = Quantity(x, Dimension('m**2'))
+        res = m*self.x_q
+        exp = Quantity(self.x, Dimension('m**2'))
         self.assertEqual(res, exp)
         
-        res = x_q*x_q
-        exp = Quantity(x*x, Dimension('m**2'))
+        res = self.x_q*self.x_q
+        exp = Quantity(self.x*self.x, Dimension('m**2'))
         self.assertEqual(res, exp)
         
-        y = uc.ufloat(0.1, 0.02)
-        res = x_q*y
-        exp = Quantity(x*y, Dimension('m'))
+        res = self.x_q*self.y
+        exp = Quantity(self.x*self.y, Dimension('m'))
         self.assertEqual(res, exp)
         
-        y_q = y*m
-        res = x_q*y_q
-        exp = Quantity(x*y, Dimension('m**2'))
+        res = self.x_q*self.y_q
+        exp = Quantity(self.x*self.y, Dimension('m**2'))
         self.assertEqual(res, exp)
         
         
     def test_pow(self):
-        x = uc.ufloat(0.20, 0.01)
-        x_q = x*m
+        x_q = self.x*m
         
-        res = x_q**2
-        exp = Quantity(x**2, Dimension('m**2'))
+        res = self.x_q**2
+        exp = Quantity(self.x**2, Dimension('m**2'))
         self.assertEqual(res, exp)
         
     def test_sin(self):
-        x = uc.ufloat(0.20, 0.01)
-        x_q = x*rad
+        x_q = self.x*rad
         # umath.sin
         from uncertainties.umath import sin
         
         # expect sin(x_q) to run smoothly as rad is an angle ?
         # and return the same value as raw x
-        sin_x = sin(x)
+        sin_x = sin(self.x)
         sin_x_q = sin(x_q)
         
         res = sin_x_q
         exp = sin_x
         self.assertEqual(res, exp)
 
-    def test_array(self):
-        x =  uc.ufloat(0.20, 0.01)
-        arr = np.array([uc.ufloat(1, 0.01),
-                        uc.ufloat(2, 0.1)])
+    def test_array_sum(self):
+        arr = np.array([self.x,
+                        self.y])
 
         arr_q =  np.array([arr[0]*m, arr[1]*m])
 
         self.assertEqual(arr.sum() *m, arr_q.sum())
 
-    def test_nominal(self):
-        x = uc.ufloat(0.20, 0.01)
-        x_q = x*m
-        
-        res = x_q.nominal_value
-        exp = x.nominal_value * m
+    def test_nominal(self):        
+        res = self.x_q.nominal_value
+        exp = self.x.nominal_value * m
         self.assertEqual(res, exp)
-        res = x_q.n
-        exp = x.n * m
+        res = self.x_q.n
+        exp = self.x.n * m
         self.assertEqual(res, exp)
         
     def test_std_dev(self):
-        x = uc.ufloat(0.20, 0.01)
-        x_q = x*m
         
-        res = x_q.std_dev
-        exp = x.std_dev * m
+        res = self.x_q.std_dev
+        exp = self.x.std_dev * m
         self.assertEqual(res, exp)
-        res = x_q.s
-        exp = x.s * m
+        res = self.x_q.s
+        exp = self.x.s * m
         self.assertEqual(res, exp)
 
 
     def test_comp(self):
-        x = uc.ufloat(0.20, 0.01)
-        y = x + 0.0001
-        
-        x_q = x*m
-        y_q = y*m
-        
-        res = y_q > x_q
-        exp = y   > x
+        res = self.y_q > self.x_q
+        exp = self.y   > self.x
         self.assertEqual(res, exp)
         
-        exp = y > 0
-        res = y_q > 0*m
+        exp = self.y   > 0
+        res = self.y_q > 0*m
         self.assertEqual(res, exp)
         
     def test_eq(self):
-        y = uc.ufloat(1, 0.1)
-        z = uc.ufloat(1, 0.1)
 
-
-        y_q = y*m
-        z_q = z*m
-
-        res = y_q == y_q
-        exp = y   == y
+        res = self.y_q == self.y_q
+        exp = self.y   == self.y
         self.assertEqual(res, exp)
         
-        res = z_q == y_q
-        exp = z   == y
+        res = self.z_q == self.y_q
+        exp = self.z   == self.y
         self.assertEqual(res, exp)
         
         
@@ -153,50 +162,41 @@ class TestWrappingUncertainties(unittest.TestCase):
         self.assertEqual(res, exp)
         
     def test_umath_sqrt(self):
-        x = uc.ufloat(0, 0)
-        x_q = x*m
-
-        res = umath.sqrt(x_q)
-        exp = umath.sqrt(x)*m**0.5
+        
+        res = umath.sqrt(self.x_q)
+        exp = umath.sqrt(self.x)*m**0.5
         self.assertEqual(res, exp)
 
     def test_neq(self):
-        x = uc.ufloat(1, 0.1)
-        y = uc.ufloat(3.14, 0.01)
-        x_q = x*m
-        y_q = y*m
         
-        res = x_q != y_q
-        exp = x != y
+        res = self.x_q != self.y_q
+        exp = self.x   != self.y
         self.assertEqual(res, exp)
 
     def test_printing(self):
-        x = uc.ufloat(1, 0.1)
-        y = uc.ufloat(3.14, 0.01)
-        x_q = x*m        
         
-        res = 'Result = {:10.2f}'.format(x_q)
-        exp = 'Result = {:10.2f}'.format(x) + " m"
+        res = 'Result = {:10.2f}'.format(self.x_q)
+        exp = 'Result = {:10.2f}'.format(self.x) + " m"
         self.assertEqual(res, exp)
 
-        res = '1 significant digit on the uncertainty:  {:.1u}'.format(x_q)
-        exp = '1 significant digit on the uncertainty:  {:.1u}'.format(x) + " m"
+        res = '1 significant digit on the uncertainty:  {:.1u}'.format(self.x_q)
+        exp = '1 significant digit on the uncertainty:  {:.1u}'.format(self.x) + " m"
         self.assertEqual(res, exp)
         
-        res = '3 significant digits on the uncertainty: {:.3u}'.format(x_q)
-        exp = '3 significant digits on the uncertainty: {:.3u}'.format(x) + " m"
+        res = '3 significant digits on the uncertainty: {:.3u}'.format(self.x_q)
+        exp = '3 significant digits on the uncertainty: {:.3u}'.format(self.x) + " m"
         self.assertEqual(res, exp)
 
-        res = '1 significant digit, exponent notation:  {:.1ue}'.format(x_q)
-        exp = '1 significant digit, exponent notation:  {:.1ue}'.format(x) + " m"
+        res = '1 significant digit, exponent notation:  {:.1ue}'.format(self.x_q)
+        exp = '1 significant digit, exponent notation:  {:.1ue}'.format(self.x) + " m"
         self.assertEqual(res, exp)
 
-        res = '1 significant digit, percentage:         {:.1u%}'.format(x_q)
-        exp = '1 significant digit, percentage:         {:.1u%}'.format(x) + " m"
+        res = '1 significant digit, percentage:         {:.1u%}'.format(self.x_q)
+        exp = '1 significant digit, percentage:         {:.1u%}'.format(self.x) + " m"
         self.assertEqual(res, exp)
 
-        res = 'Result = {:10.1e}'.format(x_q*1e-10)
-        exp = 'Result = {:10.1e}'.format(x*1e-10) + " m"
+        res = 'Result = {:10.1e}'.format(self.x_q*1e-10)
+        exp = 'Result = {:10.1e}'.format(self.x*1e-10) + " m"
         self.assertEqual(res, exp)
 
         
