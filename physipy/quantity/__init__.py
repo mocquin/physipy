afb891203b129c37970718ae0b01e51bf4473aaa
mocquin <mocquin@me.com>
2023-06-29 21:46:20 +0200

    lint

--- physipy/quantity/__init__.py
+++ physipy/quantity/__init__.py
@@ -4,7 +4,10 @@
 from .quantity import Dimension, Quantity
 from .quantity import DimensionError, SI_UNIT_SYMBOL
 from .quantity import quantify, make_quantity, dimensionify
-from .utils import check_dimension, set_favunit, dimension_and_favunit, drop_dimension, add_back_unit_param, decorate_with_various_unit, asqarray
+from .utils import (check_dimension, set_favunit,
+                    dimension_and_favunit, drop_dimension,
+                    add_back_unit_param,
+                    decorate_with_various_unit, asqarray)
 
 from .calculus import *
 
