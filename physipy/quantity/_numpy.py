da323f01638eecce4384b4da170eb564a3f271da
mocquin <mocquin@me.com>
2024-09-12 21:30:13 +0200

    add np.trapezoid support

--- physipy/quantity/_numpy.py
+++ physipy/quantity/_numpy.py
@@ -627,6 +627,28 @@ def np_trapz(q, x=None, dx=1, **kwargs):
             np.trapz(q.value, x=x.value, **kwargs),
             q.dimension * x.dimension,
         )
+    
+
+try:
+    @implements(np.trapezoid)
+    def np_trapz(q, x=None, dx=1, **kwargs):
+        # if not isinstance(q.value,np.ndarray):
+        #        raise TypeError("Quantity value must be array-like to integrate.")
+        q = quantify(q)
+        if x is None:
+            dx = quantify(dx)
+            return Quantity(
+                np.trapz(q.value, x=None, dx=dx.value, **kwargs),
+                q.dimension * dx.dimension,
+            )
+        else:
+            x = quantify(x)
+            return Quantity(
+                np.trapz(q.value, x=x.value, **kwargs),
+                q.dimension * x.dimension,
+            )
+except:
+    pass
 
 
 @implements(np.transpose)
